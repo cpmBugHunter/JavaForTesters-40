34f4ca28a624294399f86eea02fc4ab4bda2bb35
cpmBugHunter <ProfessorLys@ya.ru>
2017-04-09 12:45:41 +0300

    Решение п.4 задания 2 лекции 1

--- sandbox/src/main/java/ru/pft40/bugHunter/geometry/Point.java
+++ sandbox/src/main/java/ru/pft40/bugHunter/geometry/Point.java
@@ -12,4 +12,14 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public double calculateDistanceTo(Point p){
+        double xSubstrDegree2 = substractionDegree2(p.x, this.x);
+        double ySubstrDegree2 = substractionDegree2(p.y, this.y);
+        return Math.sqrt(xSubstrDegree2 + ySubstrDegree2);
+    }
+
+    public static double substractionDegree2(float a, float b){
+        return (a*a - 2*a*b + b*b);
+    }
 }
