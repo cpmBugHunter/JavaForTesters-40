d040a87bb2698a6c3d759cf1ff036fef94c70ed6
cpmBugHunter <ProfessorLys@ya.ru>
2017-04-15 17:38:24 +0300

    Task #4 completed

---
+++ addressbook-web-tests/src/test/java/ru/pft40/bugHunter/UserTests.java
@@ -0,0 +1,89 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class UserTests {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void UserTests() {
+        login("secret", "admin");
+        initUserCreation();
+        fillUserData("Yevgen", "Lysenko", "bughunter", "Some Company LTD",
+                "+7(909)123-45-89", "bughunter@mail.com");
+        submitUserCreation();
+        goToHomePage();
+    }
+
+    private void goToHomePage() {
+        wd.findElement(By.id("logo")).click();
+    }
+
+    private void submitUserCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillUserData(String Name, String lastName, String nickName, String company, String mobilePhone, String eMail) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(Name);
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(lastName);
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(nickName);
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(company);
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys();
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(mobilePhone);
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(eMail);
+    }
+
+    private void initUserCreation() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    private void login(String password, String username) {
+        wd.get("http://www.addressbook.loc/index.php");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
